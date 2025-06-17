0211cf1c43c7bb0bd5aa0173426b3dcbd5405453
Yash <yashgarg04072001@gmail.com>
2025-06-17 18:52:05 +0530

    updated

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
